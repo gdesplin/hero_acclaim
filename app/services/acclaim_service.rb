5338cdb6b04e0acaebe2e76fc7dd7a2ec8e3bdc0
Gavin Esplin <gdesplin@gmail.com>
2020-08-04 21:50:25 -0600

    Base acclaim service setup

---
+++ app/services/acclaim_service.rb
@@ -0,0 +1,52 @@
+
+class AcclaimService
+  require 'faraday'
+  def base_url
+    "https://sandbox-api.youracclaim.com/v1/organizations/#{ENV['ACCLAIM_ORG_ID']}"
+  end
+
+  def base_connection
+    Faraday.new(url: base_url, headers: headers) do |conn|
+      conn.adapter Faraday.default_adapter
+      conn.basic_auth(auth[:username], auth[:password])
+    end
+  end
+
+  def get_badge_templates
+    connection = base_connection
+    response = connection.get('badge_templates')
+    response
+  end
+
+  def get_badge_template(template_id)
+    connection = base_connection
+    connection.get("badge_templates/#{template_id}")
+  end
+
+  def get_issued_badges(data)
+    connection = base_connection
+    connection.get('badges') do |req|
+      req.body = data.to_json
+    end
+
+  end
+
+  def issue_badge(data)
+    # required: recipient_email, issued_to_first_name, issued_to_last_name, badge_template_id, issued_at
+    connection = base_connection
+    connection.post('badges') do |req|
+      req.body = data.to_json
+    end
+  end
+
+  private
+
+  def auth
+    { username: ENV['ACCLAIM_AUTH_TOKEN'], password: '' }
+  end
+
+  def headers
+    { 'Content-Type' => 'application/json' }
+  end
+
+end
